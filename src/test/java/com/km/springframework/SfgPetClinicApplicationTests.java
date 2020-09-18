1b4c40d9e6d8fda31bf2518988835ddda4be05bc
Kmeneouali <khalledmeneouali89@gmail.com>
2020-09-18 15:18:14 +0200

    Initial Project Add

---
+++ src/test/java/com/km/springframework/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.km.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
