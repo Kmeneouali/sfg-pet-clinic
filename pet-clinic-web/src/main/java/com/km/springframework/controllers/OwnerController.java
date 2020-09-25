723be6ef5da92b03297ba4cc54ad26e3f95d41ba
Kmeneouali <khalledmeneouali89@gmail.com>
2020-09-25 17:11:33 +0200

    add Owners Page and controller .close #14

--- pet-clinic-web/src/main/java/com/km/springframework/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/km/springframework/controllers/OwnerController.java
@@ -1,13 +1,14 @@
 package com.km.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@RequestMapping("/owners")
 @Controller
-public class IndexController {
+public class OwnerController {
 
     @RequestMapping({"","/","/index","/index.html"})
-    public String index(){
-        return "index";
+    public String listOwners(Model model){
+        return  "/owners/index";
     }
 }
--- pet-clinic-web/src/main/java/com/km/springframework/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/km/springframework/controllers/VetController.java
@@ -3,11 +3,11 @@ package com.km.springframework.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@RequestMapping("/verts")
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/index","vets/index.html"})
+    @RequestMapping({"/","/index","/index.html"})
     public String listVets(Model model){
 //        model.addAttribute("listVets");
         return "vets/index";
