bad670b56a628ce2a0d8b71fe2dc4ff23fe31ca7
Kmeneouali <khalledmeneouali89@gmail.com>
2020-09-27 10:29:42 +0200

    adding bootstrap for data.close #15

--- pet-clinic-data/src/main/java/com/km/springframework/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/km/springframework/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package com.km.springframework.services.map;
 
 import com.km.springframework.model.Pet;
-import com.km.springframework.services.CrudService;
+import com.km.springframework.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet,Long> {
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/com/km/springframework/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/km/springframework/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package com.km.springframework.services.map;
 
 import com.km.springframework.model.Vet;
-import com.km.springframework.services.CrudService;
+import com.km.springframework.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
