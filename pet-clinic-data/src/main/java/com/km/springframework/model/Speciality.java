4c140e9721c2b2dcf4e7c0c9247cce1394dadeec
Kmeneouali <khalledmeneouali89@gmail.com>
2020-09-28 13:21:34 +0200

    added specialities,  added to vet .close #28

---
+++ pet-clinic-data/src/main/java/com/km/springframework/model/Speciality.java
@@ -0,0 +1,16 @@
+package com.km.springframework.model;
+
+import com.sun.xml.internal.rngom.parse.host.Base;
+
+public class Speciality extends Base {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
