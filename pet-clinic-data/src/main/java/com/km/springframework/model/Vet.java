034f630a99181661a84a04883645b05cc187e7ab
Kmeneouali <khalledmeneouali89@gmail.com>
2020-10-24 15:25:37 +0200

    update vet with JPA mappings . Closes #38

--- pet-clinic-data/src/main/java/com/km/springframework/model/Vet.java
+++ pet-clinic-data/src/main/java/com/km/springframework/model/Vet.java
@@ -1,10 +1,14 @@
 package com.km.springframework.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
-
+@ManyToMany
+@JoinTable(name = "vert_specialities",joinColumns = @JoinColumn(name = "vert_id"),
+inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities= new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
