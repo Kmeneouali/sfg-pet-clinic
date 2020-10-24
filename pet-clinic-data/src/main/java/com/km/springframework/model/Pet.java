ed00e8f11ebd63b2d04bf8a9faf9b858f4b49431
Kmeneouali <khalledmeneouali89@gmail.com>
2020-10-24 15:33:42 +0200

    added visit entity  . Closes #39

--- pet-clinic-data/src/main/java/com/km/springframework/model/Pet.java
+++ pet-clinic-data/src/main/java/com/km/springframework/model/Pet.java
@@ -2,6 +2,9 @@ package com.km.springframework.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "pets")
 public class Pet extends  BaseEntity{
@@ -18,6 +21,9 @@ public class Pet extends  BaseEntity{
     @ManyToOne
     @JoinColumn(name ="type_id")
     private PetType petType;
+
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
+    private Set<Visit> visits=new HashSet<>();
 
     public String getName() {
         return name;
